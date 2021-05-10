eb2d1c470a57856598d1bd1da9ae49247d7425df
Marco Ravelo <mravelo5874@gmail.com>
2021-05-09 23:12:42 -0500

    added slash tests

--- ParserTester.py
+++ ParserTester.py
@@ -4,6 +4,7 @@ import sys
 from ChordParser import ChordParser
 from tests.triad_tests import triad_tests
 from tests.seventh_tests import seventh_tests
+from tests.slash_tests import slash_tests
 
 class ParserTester:
     def __init__(self):
@@ -42,6 +43,26 @@ class ParserTester:
 
         # go through each test and assert output is correct
         for input_, output_ in seventh_tests.items():
+            res = parser.parse(input_)
+            if (res != output_):
+                print ('Test %i/%i failed.\ninput: %s\nexpected output: %s\ncurrent output: %s' % (count, total_tests, input_, output_, res))
+                print ('--------------------------------')
+                sys.exit(0)
+            count += 1
+        print ("%i/%i tests completed successfully!" % (count, total_tests))
+        print ('--------------------------------')
+
+
+        # --------------------------------
+        #   SLASH TESTS
+        # --------------------------------
+        print ('--------------------------------')
+        print ("Starting slash tests...")
+        count = 0
+        total_tests = len(slash_tests)
+
+        # go through each test and assert output is correct
+        for input_, output_ in slash_tests.items():
             res = parser.parse(input_)
             if (res != output_):
                 print ('Test %i/%i failed.\ninput: %s\nexpected output: %s\ncurrent output: %s' % (count, total_tests, input_, output_, res))
