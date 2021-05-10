707207fd89fbbaa9fda74d4bd5d687a8602a8271
Marco Ravelo <mravelo5874@gmail.com>
2021-05-09 23:03:42 -0500

    reorganized tests

--- ParserTester.py
+++ ParserTester.py
@@ -2,7 +2,8 @@
 
 import sys
 from ChordParser import ChordParser
-from tests import tests
+from tests.triad_tests import triad_tests
+from tests.seventh_tests import seventh_tests
 
 class ParserTester:
     def __init__(self):
@@ -10,20 +11,44 @@ class ParserTester:
 
     def begin_tests(self):
         parser = ChordParser()
+
+        # --------------------------------
+        #   TRIAD TESTS
+        # --------------------------------
+        print ('--------------------------------')
+        print ("Starting triad tests...")
         count = 0
-        total_tests = len(tests)
+        total_tests = len(triad_tests)
 
         # go through each test and assert output is correct
-        for input_, output_ in tests.items():
+        for input_, output_ in triad_tests.items():
             res = parser.parse(input_)
             if (res != output_):
-                print ('--------------------------------')
                 print ('Test %i/%i failed.\ninput: %s\nexpected output: %s\ncurrent output: %s' % (count, total_tests, input_, output_, res))
                 print ('--------------------------------')
                 sys.exit(0)
             count += 1
-
-        print ('--------------------------------')
         print ("%i/%i tests completed successfully!" % (count, total_tests))
         print ('--------------------------------')
+
+
+        # --------------------------------
+        #   SEVENTH TESTS
+        # --------------------------------
+        print ('--------------------------------')
+        print ("Starting seventh tests...")
+        count = 0
+        total_tests = len(seventh_tests)
+
+        # go through each test and assert output is correct
+        for input_, output_ in seventh_tests.items():
+            res = parser.parse(input_)
+            if (res != output_):
+                print ('Test %i/%i failed.\ninput: %s\nexpected output: %s\ncurrent output: %s' % (count, total_tests, input_, output_, res))
+                print ('--------------------------------')
+                sys.exit(0)
+            count += 1
+        print ("%i/%i tests completed successfully!" % (count, total_tests))
+        print ('--------------------------------')
+
         sys.exit(0)
